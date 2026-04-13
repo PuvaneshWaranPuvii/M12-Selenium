7788e1e46220208c44d45da3c4a16b23fbab3d54
ADMIN <ADMIN@DESKTOP-P1FTC9P>
2026-04-13 12:14:08 +0530

    Git Confilict

--- src/test/java/demo/Practice4.java
+++ src/test/java/demo/Practice4.java
@@ -6,8 +6,11 @@ public class Practice4 {
 		System.out.println("Bhuvi went to ...");
 		System.out.println("coimbatore");
 		
-		System.out.println("Bhuvi hii...");
-		System.out.println("Welcome");
+		System.out.println("Bhuvi FE");
+		System.out.println("Bhuvi Commit");
+		System.out.println("Bhuvi ATE");
+		
+		System.out.println("ttrfvnnn");
 	}
 
 }
